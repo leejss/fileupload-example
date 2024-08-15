15706e85b2a1883c64b310e2cefd8a068798e32a
leejss <goldemshine@gmail.com>
2024-08-15 15:18:50 +0900

    frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,23 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+console.log("Start main.ts");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const fileInput = document.getElementById("file-input") as HTMLInputElement;
+const uploadButton = document.getElementById("upload-button") as HTMLButtonElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+fileInput.addEventListener("change", async () => {
+  const file = fileInput.files?.[0];
+  if (!file) return;
+
+  // Send formdata to the server
+  const formData = new FormData();
+  formData.append("file", file);
+  try {
+    const res = await fetch("http://localhost:8000", {
+      method: "POST",
+      body: formData,
+    });
+    const json = await res.json();
+    console.log("Response.json", json);
+  } catch (error) {
+    console.error("Error", error);
+  }
+});
